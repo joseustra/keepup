b80f78530e3a2bc2e33e7a3977633589a5404263
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2017-01-25 08:48:15 -0300

    Add ability to specify config file

--- cmd/root.go
+++ cmd/root.go
@@ -65,6 +65,10 @@ func init() {
 func initConfig() {
 	if cfgFile != "" { // enable ability to specify config file via flag
 		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.SetConfigName(".keepup") // name of config file (without extension)
+		viper.AddConfigPath("$HOME")   // adding home directory as first search path
+		viper.AutomaticEnv()           // read in environment variables that match
 	}
 
 	// If a config file is found, read it in.
